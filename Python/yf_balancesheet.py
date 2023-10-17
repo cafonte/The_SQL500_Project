d4819910e6fcab9251ef56f9fa70c89382727204
Chase F <109887258+cafonte@users.noreply.github.com>
2023-10-17 07:45:16 -0400

    Update yf_balancesheet.py
    + Updated to use latest date from SQL

--- Python/yf_balancesheet.py
+++ Python/yf_balancesheet.py
@@ -14,7 +14,7 @@ sp500source = finsymbols.get_sp500_symbols()
 sp500 = [s['symbol'].replace('\n','') for s in sp500source] # Clean wiki output for loop
 print("S&P500 List Acquired..")
 # Execute a SQL query and load the results into a DataFrame
-db_df = pd.read_sql_query('SELECT symbol, last_updated FROM "balance_sheet"', con=engine)
+db_df = pd.read_sql_query('SELECT symbol, max(last_updated) FROM "balance_sheet" group by symbol', con=engine)
 print("DB Balance Sheet Acquired.."+"\n")
 tickercount = 0
 num_skipped = 0
